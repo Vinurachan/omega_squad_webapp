47d996186446149a5227f37b724c32685435531b
Vinurachan <vinura.18@itfac.mrt.ac.lk>
2021-09-14 17:48:56 +0530

    HomePage Navbar edited

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
+import React from "react";
 import { Route, Switch } from "react-router-dom";
-
-import logo from './logo.svg';
-import './App.css';
+import HomePage from "./pages/HomePage";
+import "./App.css";
 
 const App = () => {
   return (
     <main id="main">
-        <section id="hero" className="d-flex align-items-center"></section>
-        <Switch>
-          <Route exact path="/">
-            <HomePage />
-          </Route>
-          
-        </Switch>
-      </main>
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+      </Switch>
+    </main>
   );
-}
+};
 
 export default App;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,18 +1,11 @@
-import React, { useContext } from "react";
-import { useHistory } from "react-router";
-import { AuthContext } from "../AuthContext/AuthContext";
-import "./HomePage.css";
+import React from "react";
+import NavBar from "../components/NavBar";
 
 const HomePage = () => {
-//   const history = useHistory();
-
-//   const user = useContext(AuthContext);
-
-//   const isSignedIn = user !== "INIT" && user;
-
   return (
-    <div>
-        <h1>Home Page</h1>
+    <div className="container">
+      <h1>TMS</h1>
+      <NavBar/>
     </div>
   );
 };
