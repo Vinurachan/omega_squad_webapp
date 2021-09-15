4bcdd7211f668ed1aacc6df472d82d5988489ada
ManishyamAbeyweera <manishyam.rc@gmail.com>
2021-09-15 18:19:43 +0530

    firebase integration done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import "./App.css";
+import {db} from "./firebase/firebase";
 
 const App = () => {
   return (
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,31 @@
+import { collection, getDocs } from "firebase/firestore";
 import React from "react";
 import NavBar from "../components/NavBar";
+import { db } from "../firebase/firebase";
 
 const HomePage = () => {
+  const fetchUser = async () => {
+    const querySnapshot = await getDocs(collection(db, "users"));
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id);
+      console.table(doc.data());
+    });
+  };
+
   return (
     <div className="container">
-      <h1>TMS</h1>
-      <NavBar/>
+      <NavBar />
+      <div style={{ marginTop: 100 }}>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            fetchUser();
+          }}
+          type="Submit"
+        >
+          Retrieve
+        </button>
+      </div>
     </div>
   );
 };
